cd1961f43fd7abd7df30abf1fd98feb2100ea30e
Mmaphuti K Madiope <CodeWithMmaphuti@gmail.com>
2025-08-29 21:47:25 +0200

    Add files via upload

---
+++ Portfolio/style/script/script.js
@@ -0,0 +1,26 @@
+/*.home .home-container .btn-sci .btn{
+    position: relative;
+    text-decoration: none;
+    border: 1px solid red;
+    border-radius: 50px;
+    border-spacing: 15px;
+    color: blue;
+    font-size: 20px;
+    background: red;
+    box-shadow: 0 0 10px red;
+}
+
+.home .home-container .btn-sci .sci{
+    display: flex;
+    align-items: center;
+    position: absolute;
+    right: 1000px;
+    margin: -35px;
+}
+
+.home .home-container .btn-sci .sci a{
+    padding: 7px;
+    border: 1px solid white;
+    border-radius: 50%;
+}
+    */
